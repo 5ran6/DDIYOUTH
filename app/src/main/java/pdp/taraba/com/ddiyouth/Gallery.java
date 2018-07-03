4cf264eb931abfcb7c08acac7a7135945064650d
5ran6 <5raan6@gmail.com>
2018-07-03 05:39:07 +0100

    All elements added

---
+++ app/src/main/java/pdp/taraba/com/ddiyouth/Gallery.java
@@ -0,0 +1,115 @@
+package pdp.taraba.com.ddiyouth;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
+
+import com.github.paolorotolo.appintro.AppIntro;
+
+/**
+ * Created by 5ran6 on 26/03/18.
+ */
+public class Gallery extends AppIntro {
+    private int PERMISSION = 1;
+
+    // DID NOT override onCreate. Used init
+    @Override
+    public void init(Bundle savedInstanceState) {
+        if (ContextCompat.checkSelfPermission(Gallery.this, Manifest.permission.VIBRATE) == PackageManager.PERMISSION_GRANTED) {
+//            Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+
+        } else {
+            requestVibrationPermission();
+        }
+        //adding the slides
+        addSlide(AppIntroSlider.newInstance(R.layout.app_intro1));
+        addSlide(AppIntroSlider.newInstance(R.layout.app_intro2));
+        addSlide(AppIntroSlider.newInstance(R.layout.app_intro3));
+        addSlide(AppIntroSlider.newInstance(R.layout.app_intro4));
+        addSlide(AppIntroSlider.newInstance(R.layout.app_intro5));
+        addSlide(AppIntroSlider.newInstance(R.layout.app_intro6));
+        addSlide(AppIntroSlider.newInstance(R.layout.app_intro7));
+
+        // Show and Hide Skip and Done buttons
+        showStatusBar(false);
+        showSkipButton(false);
+
+        // Turn vibration on and set intensity
+        // You will need to add VIBRATE permission in Manifest file
+        setVibrate(true);
+        setVibrateIntensity(30);
+
+        //Add animation to the intro slider
+        setDepthAnimation();
+    }
+
+
+    private void requestVibrationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.VIBRATE)) {
+            new AlertDialog.Builder(this).setTitle("Permission needed").setCancelable(false).setMessage("Permission needed for this activity").setPositiveButton("Permit", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCompat.requestPermissions(Gallery.this, new String[]{Manifest.permission.VIBRATE}, PERMISSION);
+
+                }
+            }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.VIBRATE}, PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
+    @Override
+    public void onSkipPressed() {
+        // Do something here when users click or tap on Skip button.
+
+        finish();
+
+//          MENU LOGIN
+//          send the intent extras to the HomeActivity for opening login Fragment
+//          Intent intent = new Intent(this, HomeActivity.class);
+//          startActivity(intent);
+
+    }
+
+    @Override
+    public void onNextPressed() {
+        // Do something here when users click or tap on Next button.
+    }
+
+    @Override
+    public void onDonePressed() {
+        // Do something here when users click or tap tap on Done button.
+        finish();
+//        Intent intent = new Intent(this, LoginActivity.class);
+//        startActivity(intent);
+    }
+
+    @Override
+    public void onSlideChanged() {
+        // Do something here when slide is changed
+    }
+}
---
+++ app/src/main/java/pdp/taraba/com/ddiyouth/HomeActivity.java
@@ -0,0 +1,225 @@
+package pdp.taraba.com.ddiyouth;
+
+/**
+ * Created by 5ran6 on 24/10/17.
+ */
+
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class HomeActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+    //   public ImageView ;
+    String name;
+    String email;
+    byte[] image;
+    FrameLayout frameLayout;
+    //  private SharedPreferences mSharedPreferences;
+    Class fragmentClass = Home.class;
+    Fragment myFragment = (Fragment) fragmentClass.newInstance();
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private TextView currentTextView;
+
+    public HomeActivity() throws IllegalAccessException, InstantiationException {
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage("What do you want to do?")
+                .setCancelable(false)
+                .setPositiveButton("Close app", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //                       Instruction.this.finish();
+                        finishAffinity();
+                    }
+                })
+                .setNegativeButton("CONTINUE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//                        Intent i = new Intent(getApplicationContext(), Login.class);
+                        //                      startActivity(i);
+                    }
+                })
+                .show();
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        if (savedInstanceState != null) {
+            //Restore the fragment's instance
+            //       myFragment = getSupportFragmentManager().getFragment(savedInstanceState, myFragment.toString());
+
+        }
+
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nv);
+        // View img = navigationView.getHeaderView(0);
+
+        //img_cover = (ImageView) findViewById(R.id.backdrop);
+        frameLayout = (FrameLayout) findViewById(R.id.flcontent);
+        //mainLay = (RelativeLayout) findViewById(R.id.main);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setUpDrawerContent(navigationView);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flcontent, myFragment).commit();
+
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        // handle the preference change here i.e on delete
+//        loadArray();
+//        saveArray();
+//        adapter.notifyDataSetChanged();
+    }
+
+
+    /**
+     * Initializing collapsing toolbar
+     * Will show and hide the toolbar title on scroll
+     */
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        //Save the fragment's instance
+//        savedInstanceState.putString("fragClassName", fragmentClass.toString());
+//        final Fragment fragmentInFrame = getSupportFragmentManager().findFragmentById(R.id.flcontent);
+//        getSupportFragmentManager().putFragment(outState, fragmentInFrame.getTag(), myFragment);
+////        getSupportFragmentManager().putFragment(outState, "myFragmentName", myFragment);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+// When orientation is changed, activity goes through onResume()
+        try {
+            myFragment = (Fragment) fragmentClass.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+//        String fragClassName = savedInstanceState.getString("fragClassName", "Home");
+//        try {
+//            Class frag = Class.forName(fragClassName);
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            myFragment = (Fragment) frag.newInstance();
+//        } catch (InstantiationException e) {
+//            e.printStackTrace();
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+    private void setUpDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectItemDrawer(item);
+                return true;
+            }
+        });
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void selectItemDrawer(MenuItem menuItem) {
+        myFragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.home:
+                fragmentClass = Home.class;
+                break;
+            case R.id.nycn:
+                fragmentClass = NYCN.class;
+                break;
+            case R.id.ddi:
+                fragmentClass = DDI.class;
+                break;
+            case R.id.deputy:
+                fragmentClass = Deputy.class;
+                break;
+            case R.id.news:
+                fragmentClass = News.class;
+                break;
+            case R.id.contact:
+                fragmentClass = Contact.class;
+                break;
+            case R.id.exit:
+                finishAffinity();
+                break;
+            default:
+                // fragmentClass = HomeActivity.class;
+        }
+        try {
+            myFragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flcontent, myFragment).commit();
+        menuItem.setChecked(true);
+        setTitle(menuItem.getItemId());
+        mDrawerLayout.closeDrawers();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void cont(View view) {
+        Intent g = new Intent(getApplicationContext(), Gallery.class);
+        Toast.makeText(getApplicationContext(), "For now we shall open this. No contents for the original page", Toast.LENGTH_LONG).show();
+        startActivity(g);
+    }
+
+    public void gallery(View view) {
+        Intent g = new Intent(getApplicationContext(), Gallery.class);
+        startActivity(g);
+    }
+}
